f13b706e7da2053d3dd10adc808622c1484d4e77
DavidJ0707 <133535508+DavidJ0707@users.noreply.github.com>
2024-11-11 10:35:41 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    document.getElementById('hamburger').addEventListener('click', function() {
+        const nav = document.getElementById('nav-menu');
+        nav.classList.toggle('active');
+    });
+</script>
